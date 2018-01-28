c22ceb01895f9045f13d7fad38586e39b975b251
czechben <czechben@umich.edu>
2018-01-28 18:22:01 -0500

    audio

--- mp3_to_wav_converter.py
+++ mp3_to_wav_converter.py
@@ -3,6 +3,8 @@ from pydub import AudioSegment
 import soundfile as sf
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from pyAudioAnalysis import audioBasicIO
+from pyAudioAnalysis import audioFeatureExtraction
 
 # TO RUN: python mp3_to_wav_converter.py song_name(without.mp3 at the end)
 
@@ -17,7 +19,13 @@ song = AudioSegment.from_mp3(song_name_mp3)
 
 song.export(song_name_wav, format = "wav")
 
-sig, samplerate = sf.read(song_name_wav)
+[Fs, x] = audioBasicIO.readAudioFile(song_name_wav);
+F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
+plt.subplot(2,1,1); plt.plot(F[0,:]); plt.xlabel('Frame no'); plt.ylabel('ZCR'); 
+plt.subplot(2,1,2); plt.plot(F[1,:]); plt.xlabel('Frame no'); plt.ylabel('Energy'); plt.show()
+
+
+#sig, samplerate = sf.read(song_name_wav)
 
 #print(sig.shape)
 #plt.plot(sig);
