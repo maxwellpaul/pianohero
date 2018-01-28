1fd7fd27406f40f923e6e7d8e231f9db6510cbe5
paulmax95 <paulmax@umich.edu>
2018-01-28 17:14:35 -0500

    Alpha controller done

--- controller.py
+++ controller.py
@@ -1,12 +1,27 @@
 from model import Model
-from pynput import keyboard
-
-# https://pypi.python.org/pypi/pynput
+import click
 
 class Controller:
 
     def addObserver(self, model):
         self._model = model
 
+    def listenAndCall(self):
+        while True:
+            key = click.getchar()
+            if (key == 'q'):
+                return
+            elif (key == 'a'):
+                self._model.playNoteA()
+            elif (key == 's'):
+                self._model.playNoteB()
+            elif (key == 'd'):
+                self._model.playNoteC()
+            elif (key == 'f'):
+                self._model.playNoteD()
+
+            
     def run(self):
         print ("Running controller")
+        self.listenAndCall()
+        print ("Controller quitting")
