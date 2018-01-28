f3e4698188739d7817c2deaa881775db88e8470b
Josh Knox <jtknox@Joshs-MacBook-Pro.local>
2018-01-27 21:18:25 -0500

    UI tutorial vid 1

---
+++ PianoHero/Assets/Activator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//An activator is the circle at the bottom that notes pass through
+public class Activator : MonoBehaviour {
+
+	//Whatever key the activator is linked to
+	public KeyCode key;
+
+	//To determine if a note is inside the activator
+	bool active = false;
+
+	//The game object inside the activator
+	GameObject note;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//If the intended key is pressed and there is a note inside the activator
+		if (Input.GetKeyDown (key) && active) {
+			Destroy (note);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		active = true;
+		//If there is a note inside the activator, grab its game object
+		if (col.gameObject.tag == "Note") {
+			note = col.gameObject;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		active = false;
+	}
+}
---
+++ PianoHero/Assets/Note.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+
+	//The Rigidbody2D class essentially provides the same functionality in 2D that the Rigidbody class provides in 3D. 
+	//Adding a Rigidbody2D component to a sprite puts in under the control of the physics engine. 
+	//By itself, this means that the sprite will be affected by gravity and can be controlled from scripts using forces. 
+	//By adding the appropriate collider component, the sprite will also respond to collisions with other sprites.
+	Rigidbody2D rb;
+	public float speed;
+
+	//Awake is used to initialize any variables or game state before the game starts. 
+	//Awake is called only once during the lifetime of the script instance. 
+	//Awake is called after all objects are initialized
+	void Awake(){
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		rb.velocity = new Vector2 (0, -speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
