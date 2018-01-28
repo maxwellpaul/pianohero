7b1274c82c4cebb8287c14b9d7851fad5c8519be
averubin <averubin@umich.edu>
2018-01-28 18:04:56 -0500

    added mp3 to wav conversion and visualization of wav file

--- mp3_to_wav_converter.py
+++ mp3_to_wav_converter.py
@@ -1,7 +1,24 @@
+import pydub, sys, os
 from pydub import AudioSegment
+import soundfile as sf
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
-song = AudioSegment.from_mp3("test.mp3")
-
-song.export("test.wav", format = "wav")
+mpl.rcParams['agg.path.chunksize'] = 10000
 
 
+
+song_name_mp3 = str(sys.argv[1]) + ".mp3"
+song_name_wav = "wav_files/" + str(sys.argv[1]) + ".wav"
+
+pydub.AudioSegment.converter = r"/usr/local/Cellar/ffmpeg/3.4.1/bin/ffmpeg"
+
+song = AudioSegment.from_mp3(song_name_mp3)
+
+song.export(song_name_wav, format = "wav")
+
+sig, samplerate = sf.read(song_name_wav)
+
+print(sig.shape)
+#plt.plot(sig);
+#plt.show()
