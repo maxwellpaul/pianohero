edc9c42c1a6a922b705760a0a51992da75bfdddd
Paul Maxwell <paulmax@umich.edu>
2018-01-28 16:31:48 -0500

    Delete dependencies.sh

--- dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# NOTE: if you can't run this, run "chmod 755 dependencies.sh", and then "./dependencies.sh"
-echo 'Installing all dependencies for Piano Hero'
-
-echo 'Installing pynput...'
-python -m pip install pynput
