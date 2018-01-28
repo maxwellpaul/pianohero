7a5b5336451089e9c0812722d125b762e7fa92d1
paulmax95 <paulmax@umich.edu>
2018-01-28 17:18:31 -0500

    added comments, cleaned up controller

--- controller.py
+++ controller.py
@@ -1,11 +1,15 @@
 from model import Model
 import click
 
+# Controller class handles all input for the game, and relays information to the model
 class Controller:
 
+    # Attach the controller to the model as an observer
     def addObserver(self, model):
         self._model = model
 
+    # Listen for keyboard inputs and relay them to the model
+    # Letter 'q' signifies quitting the game, and will end the loop
     def listenAndCall(self):
         while True:
             key = click.getchar()
@@ -19,8 +23,8 @@ class Controller:
                 self._model.playNoteC()
             elif (key == 'f'):
                 self._model.playNoteD()
-
-            
+    
+    # Run the controller
     def run(self):
         print ("Running controller")
         self.listenAndCall()
