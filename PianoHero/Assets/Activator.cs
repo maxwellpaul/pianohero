30708d4d4eb4376ac6d362c3397c20efb1a8fa95
Josh Knox <jtknox@Joshs-MacBook-Pro.local>
2018-01-28 17:36:59 -0500

    Second tutorial vid

--- PianoHero/Assets/Activator.cs
+++ PianoHero/Assets/Activator.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 //An activator is the circle at the bottom that notes pass through
 public class Activator : MonoBehaviour {
 
+	SpriteRenderer sr;
+	Color old;
+
 	//Whatever key the activator is linked to
 	public KeyCode key;
 
@@ -15,13 +18,21 @@ public class Activator : MonoBehaviour {
 	GameObject note;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		sr = GetComponent<SpriteRenderer> ();
 	}
-	
+
+	void Start() {
+		old = sr.color;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		//If the intended key is pressed and there is a note inside the activator
+		if (Input.GetKeyDown (key)) {
+			StartCoroutine (Pressed ());
+		}
+
 		if (Input.GetKeyDown (key) && active) {
 			Destroy (note);
 		}
@@ -37,5 +48,11 @@ public class Activator : MonoBehaviour {
 
 	void OnTriggerExit2D(Collider2D col) {
 		active = false;
+	}
+
+	IEnumerator Pressed() {
+		sr.color = new Color (0, 0, 0);
+		yield return new WaitForSeconds (0.05f);
+		sr.color = old;
 	}
 }
