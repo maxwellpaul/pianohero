e71ac5e2c5bd65e8fb99810ba2d308c3ae09a7fa
averubin <averubin@umich.edu>
2018-01-28 19:27:52 -0500

    pulling

--- mp3_to_wav_converter.py
+++ mp3_to_wav_converter.py
@@ -1,10 +1,11 @@
-import pydub, sys, os
+import pydub, sys, os, wave
+#import sage.media
+import numpy as np
+import scipy.io.wavfile
 from pydub import AudioSegment
 import soundfile as sf
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from pyAudioAnalysis import audioBasicIO
-from pyAudioAnalysis import audioFeatureExtraction
 
 # TO RUN: python mp3_to_wav_converter.py song_name(without.mp3 at the end)
 
@@ -19,11 +20,22 @@ song = AudioSegment.from_mp3(song_name_mp3)
 
 song.export(song_name_wav, format = "wav")
 
-[Fs, x] = audioBasicIO.readAudioFile(song_name_wav);
-F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
-plt.subplot(2,1,1); plt.plot(F[0,:]); plt.xlabel('Frame no'); plt.ylabel('ZCR'); 
-plt.subplot(2,1,2); plt.plot(F[1,:]); plt.xlabel('Frame no'); plt.ylabel('Energy'); plt.show()
 
+# FIGURE OUT HOW TO USE SAGE!!
+#songg = sage.media.wav.Wave(song_name_wav)
+
+#rate, data = scipy.io.wavfile.read(song_name_wav)
+
+#print (data)
+
+#plt.plot(data)
+#plt.show()
+
+#print(song_data.getnchannels())
+#frames = song_data.readframes(song_data.getnframes())
+#print(type(frames))
+#plt.plot(frames)
+#plt.show()
 
 #sig, samplerate = sf.read(song_name_wav)
 
