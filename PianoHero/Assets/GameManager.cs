8b0d87814cd1c0f0457a15f622fe99c7b1b77e3d
Josh Knox <jtknox@Joshs-MacBook-Pro.local>
2018-01-30 13:20:51 -0500

    began 5th tut

---
+++ PianoHero/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public int multiplier = 1;
+	public int streak = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+	}
+
+	public int GetScore() {
+		return 100 * multiplier;
+	}
+}
