a0952f7ac173e275701750d3f4cd69484638123c
paulmax95 <paulmax@umich.edu>
2018-01-30 16:16:17 -0500

    added mult and streak

--- PianoHero/Assets/GameManager.cs
+++ PianoHero/Assets/GameManager.cs
@@ -6,10 +6,12 @@ public class GameManager : MonoBehaviour {
 
 	public int multiplier = 1;
 	public int streak = 0;
+	public int mult_length = 10;
 
 	// Use this for initialization
 	void Start () {
-		
+		PlayerPrefs.SetInt ("Score", 0);
+		UpdateGUI();
 	}
 	
 	// Update is called once per frame
@@ -17,7 +19,25 @@ public class GameManager : MonoBehaviour {
 		
 	}
 
+	public void AddStreak() {
+		++streak;
+		multiplier = 1 + streak / mult_length;
+		UpdateGUI();
+	}
+
+	public void ResetStreak() {
+		streak = 0;
+		multiplier = 1;
+		UpdateGUI();
+	}
+
+	void UpdateGUI() {
+		PlayerPrefs.SetInt ("Streak", streak);
+		PlayerPrefs.SetInt ("Mult", multiplier);
+	}
+
 	void OnTriggerEnter2D(Collider2D col) {
+			
 	}
 
 	public int GetScore() {
