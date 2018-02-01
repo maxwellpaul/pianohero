dc7f9cf5b0eac1d0fd7e50f8441b6adf287db91a
Josh Knox <jtknox@Joshs-MacBook-Pro.local>
2018-01-31 19:36:44 -0500

    began dynamic note creation

--- PianoHero/Assets/Scripts/GameManager.cs
+++ PianoHero/Assets/Scripts/GameManager.cs
@@ -47,13 +47,47 @@ public class GameManager : MonoBehaviour {
 		return 100 * multiplier;
 	}
 
-	static void ReadString()
+	void ReadString()
     {
+        float noteOneX = -1.5f;
+        float noteTwoX = -.5f;
+		float noteThreeX = .5f;
+		float noteFourX = 1.5f;
+        float currentY = 5.7f;
+        float noteDiameter = 1;
         string path = "Assets/Songs/test.txt";
 
 		//Read the text from directly from the test.txt file
 		StreamReader reader = new StreamReader(path);
-		Debug.Log(reader.ReadToEnd());
+        int noteType;
+        while(!reader.EndOfStream) {
+            if(int.TryParse(reader.ReadLine(), out noteType)) {
+                print("successfully read note" + noteType);
+            }
+            switch(noteType) {
+                case 1:
+                    Instantiate(note, new Vector3(noteOneX, currentY, 0), Quaternion.identity);
+                    currentY += noteDiameter;
+                    break;
+                case 2:
+					Instantiate(note, new Vector3(noteTwoX, currentY, 0), Quaternion.identity);
+                    currentY += noteDiameter;
+                    break;
+                case 3:
+					Instantiate(note, new Vector3(noteThreeX, currentY, 0), Quaternion.identity);
+                    currentY += noteDiameter;
+                    break;
+                case 4:
+					Instantiate(note, new Vector3(noteFourX, currentY, 0), Quaternion.identity);
+                    currentY += noteDiameter;
+                    break;
+                default:
+                    print("Error: Invalid Note Type");
+                    break;
+            }
+        }
+        //reader.ReadLine();
+		//Debug.Log(reader.ReadToEnd());
 		reader.Close();
 	}
 }
