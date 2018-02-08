c6e119067b65c0a75e66cb9b5c030b8a936518d5
Avery Rubin <averubin@umich.edu>
2018-02-07 21:10:04 -0500

    Update feature_extract.m

--- wav_files/feature_extract.m
+++ wav_files/feature_extract.m
@@ -1,19 +1,3 @@
-%[y, fs] = audioread('Simple_Piano.wav');
-%t = linspace(0, length(y)/fs, length(y));
-%plot(t, y);
-%Nfft = 1024;
-%Nfft = 2^nextpow2(length(y));
-%f = linspace(0, fs, Nfft);
-%G = abs(fft(y, Nfft));
-%figure; plot(f(1:Nfft/2), G(1:Nfft/2));
-
-%G_vec = linspace(0, length(G)/fs, length(G));
-%[pks, locs] = findpeaks(G_vec);
-%print(pks);
-%figure; plot(f(1:Nfft/2), G(1:Nfft/2), f(locs), pks, 'or');
-%print(pks)
-%plot(pks, locs);
-
 %Change to whatever wav you want
 %reads in frequencies and converts to amplitude
 function r = feature_extract(songName)
@@ -99,7 +83,7 @@ function r = feature_extract(songName)
 
     end
     
-    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%FIX THIS%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     dividers = zeros(1, length(averages)*3);
     divider_index = 1;
     current_section = [];
@@ -121,6 +105,9 @@ function r = feature_extract(songName)
             end
         end
     end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%FIX THIS%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    
 
     timeRatios = timeRatios(timeRatios ~= 0);
     for_size = peakFreqs(peakFreqs ~= 0);
@@ -177,20 +164,3 @@ function r = feature_extract(songName)
     movefile(outFileName, '../PianoHero/Assets/Songs');
     
 end
-
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%[y2,fs2] = audioread('Simple_Piano.wav');
-%y2 = y2(:, 2);
-%spectrogram(y2, 256, [], [], fs2, 'yaxis');
-
-% create a frequency vector
-%freq = 0:fs/length(y):fs/2;
-% plot magnitude
-%plot(freq,abs(ydft=));
-% plot phase
-%plot(freq,unwrap(angle(ydft))); 
-%xlabel('Hz');
-%ydft_vect = abs(ydft);
-
-%plot(freq,abs(ydft), freq(locs), pks, 'or');
