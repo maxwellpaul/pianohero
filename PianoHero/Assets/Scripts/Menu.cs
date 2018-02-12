05d5fe53089edca4e7fa0ea4ed5eb554bc238221
paulmax95 <paulmax@umich.edu>
2018-02-12 16:57:16 -0500

    menu stuff

--- PianoHero/Assets/Scripts/Menu.cs
+++ PianoHero/Assets/Scripts/Menu.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using System.Text.RegularExpressions;
 
 public class Menu : MonoBehaviour {
 
 	public Dropdown dropdown;
+	Regex r = new Regex (@"(\.wav)", RegexOptions.IgnoreCase);
+
 	List<string> WAVFiles = new List<string> () { "Paul", "Maxwell" };
 	public string selectedWAV;
 
@@ -27,13 +30,26 @@ public class Menu : MonoBehaviour {
 	}
 
 	public void PopulateList() {
-		// read the values from the folder
+		WAVFiles.Clear ();
+		dropdown.ClearOptions ();
+
 		string currPath = Application.dataPath + "/Songs/";
 		foreach (string file in System.IO.Directory.GetFiles(currPath)) {
-			print (file);
+			string[] filenameArr = file.Split('/');
+			string filename = filenameArr [filenameArr.Length - 1];
+			if (r.IsMatch (filename)) {
+				WAVFiles.Add (filename);
+			}
 		}
 
-		List<string> songs = new List<string> () { "Paul", "Maxwell" };
+		List<string> songs = new List<string> () {};
+		foreach (string filename in WAVFiles) {
+			songs.Add (splitFilename (filename));
+		}
 		dropdown.AddOptions (songs);
+	}
+
+	string splitFilename(string filename) {
+		return filename;
 	}
 }
