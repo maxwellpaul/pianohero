8fb211bf82818b965df3ab57336ed80bfbeb7c17
Josh Knox <jtknox@Joshs-MacBook-Pro.local>
2018-03-19 19:56:50 -0400

    SHOOK

--- PianoHero/Assets/StreamingAssets/pipeline.sh
+++ PianoHero/Assets/StreamingAssets/pipeline.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
-mkdir ~/Desktop/EECS\ 498/PianoHero/pianohero/PianoHeroResources/Scripts/hello
+cd ../../..
+source 498/bin/activate
+python mp3_to_wav_converter.py
+deactivate
+cd PianoHeroResources/MP3Files
+cd ../../wav_files/
+./run_feature_extract.sh /Applications/MATLAB/MATLAB_Runtime/v93
+cd song_queue
